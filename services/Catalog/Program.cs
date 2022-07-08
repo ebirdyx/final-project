2b3465b4458bf44392cf3f3fe05e0f79071d2d63
Pamela Brahollari <ebirdyxx@gmail.com>
2022-07-08 19:32:15 -0400

    Remove comments

--- services/Catalog/Program.cs
+++ services/Catalog/Program.cs
@@ -4,10 +4,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -19,7 +16,6 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
